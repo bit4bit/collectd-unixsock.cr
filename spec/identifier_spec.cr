22705419432450a7585414a4a148a781d4f9d24c
Jovany Leandro G.C <bit4bit@riseup.net>
2021-10-21 17:28:02 -0500

    test: rename test

---
+++ spec/identifier_spec.cr
@@ -0,0 +1,37 @@
+require "./spec_helper"
+
+# see https://collectd.org/documentation/manpages/collectd-unixsock.5.shtml
+describe Collectd::Unixsock::Identifier do
+
+  it "identifier from string" do
+    ident = Collectd::Unixsock::Identifier.from_string("localhost/test/load")
+
+    ident.hostname.should eq "localhost"
+    ident.plugin.should eq "test"
+    ident.type.should eq "load"
+  end
+
+  it "identifier from string when have spaces" do
+    ident = Collectd::Unixsock::Identifier.from_string("localhost/test/load 1")
+
+    ident.hostname.should eq "localhost"
+    ident.plugin.should eq "test"
+    ident.type.should eq "load 1"
+  end
+
+  it "encode identifier" do
+    ident = Collectd::Unixsock::Identifier.from_string("localhost/test/load")
+    "localhost/test/load".should eq ident.to_s
+  end
+
+  it "quote when identifier have spaces" do
+    ident = Collectd::Unixsock::Identifier.from_string("localhost/test/load 1")
+    "\"localhost/test/load 1\"".should eq ident.to_s
+  end
+
+  it "fail with invalid identifier" do
+    expect_raises(Collectd::Unixsock::Identifier::Error) do
+      Collectd::Unixsock::Identifier.from_string("localhost/test|load")
+    end
+  end
+end
