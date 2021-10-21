96d28b6ec9e47d03f3a0a8cd4f31d7eae24f0954
Jovany Leandro G.C <bit4bit@riseup.net>
2021-10-21 17:26:12 -0500

    feat: add Identifier

--- src/collectd-unixsock.cr
+++ src/collectd-unixsock.cr
@@ -2,5 +2,31 @@
 module Collectd::Unixsock
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Identifier
+    class Error < Exception
+    end
+
+    getter hostname
+    getter plugin
+    getter type
+
+    def initialize(@hostname : String, @plugin : String, @type : String)
+    end
+
+    def self.from_string(path)
+      hostname, plugin, type_ = path.split('/', 3)
+      self.new(hostname, plugin, type_)
+    rescue
+      raise Error.new("can't parse path #{path}")
+    end
+
+    def to_s
+      out = "#{@hostname}/#{@plugin}/#{@type}"
+      if out.includes?(' ')
+        "\"#{out}\""
+      else
+        out
+      end
+    end
+  end
 end
